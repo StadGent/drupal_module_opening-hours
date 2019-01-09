5f8ae46d2ea208d769082eccf48f29ed0ac5561e
Peter Decuyper <peter.decuyper@digipolis.gent>
2019-01-09 10:54:55 +0100

    VG-1519: Added new "Broken" views filter for the opening_hours:broken field.

---
+++ src/Plugin/views/filter/Broken.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\opening_hours\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\filter\BooleanOperator;
+
+/**
+ * Filters by given list of node title options.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("opening_hours_broken")
+ */
+class Broken extends BooleanOperator {
+
+  /**
+   * Run the query to get all entities by their broken link status.
+   */
+  public function query() {
+    $this->ensureMyTable();
+    $fieldName = sprintf('%s.%s', $this->tableAlias, $this->realField);
+
+    if (empty($this->value)) {
+      $where = sprintf('(%s = 0  OR %s IS NULL)', $fieldName, $fieldName);
+    }
+    else {
+      $where = sprintf('%s <> 0', $fieldName);
+    }
+
+    $this->query->addWhereExpression($this->options['group'], $where);
+  }
+
+}
